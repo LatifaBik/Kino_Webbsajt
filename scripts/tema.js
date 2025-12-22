67c41f0a109d63046a82759968df54362d644622
latifabikndarne <latifa.bikndarne@gmail.com>
2025-12-22 17:19:31 +0100

    the file tema.scss and tema.js are added

---
+++ scripts/tema.js
@@ -0,0 +1,17 @@
+
+const toggleBtn = document.querySelector('theme-toggle');
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+
+  // spara valet
+  const theme = document.body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', theme);
+});
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark');
+}
+

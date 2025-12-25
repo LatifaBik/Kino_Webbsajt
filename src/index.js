0f8f22760611cab9528ce590cf5b9713793b75f5
elinmolindr <elinmolinder1@gmail.com>
2025-12-25 14:03:50 +0100

    Change src path to main.scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './scss/main.scss';
+import './sections/main.scss';
 import './scripts/menu.js';
-import './scss/main.scss';
+
 import { toggleMenu } from './scripts/menu.js'; 
 
 toggleMenu(); 
